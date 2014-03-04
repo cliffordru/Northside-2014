2f36e246e8f3e8a5ec56bb58f1bffc1e9b66f399
Cliff G <clifford.gray@gmail.com>
2014-03-03 22:41:16 -0500

    Update to header - site header was updated by northside and I needed to merge in changes

--- template-attendees.php
+++ template-attendees.php
@@ -155,11 +155,28 @@ function page(){
 <? get_header(); ?>
 
 <div class="socialmediabuttons">
-<a href="http://www.facebook.com/northsidefest" target="blank"><img class="smpop" style="padding-right: 3px;" src="http://northsidefestival.com/wp-content/uploads/2013/01/facebook-NSF.png"/></a><a href="http://www.twitter.com/northsidefest" target="blank"><img class="smpop" style="padding-right: 3px;" src="http://northsidefestival.com/wp-content/uploads/2013/01/twitter-nsf.png"/></a><a href="http://instagram.com/thelmagazine" class="ig-b- ig-b-48" target="blank"><img class="smpop" style="padding-right: 3px;" src="http://www.northsidefestival.com/wp-content/uploads/2013/01/instagram-NSF.png" width="38"/></a>
+<a href="http://www.facebook.com/northsidefest" class="button_facebook" target="blank">
+</a>
+<a href="http://www.twitter.com/northsidefest" class="button_twitter" target="blank">
+</a>
+<a href="http://instagram.com/thelmagazine" class="button_instagram" target="blank">
+</a>
 </div>
 
-<div style="left: 180px; margin-bottom: 0; margin-top: 0; padding-bottom: 0; position: relative; top: -330px; width: 175px;" class="fblike"><iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FNorthsideFest&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></div>
+<div style="left: 297px; margin-bottom: 0; margin-top: 0; padding-bottom: 0; position: relative; top: -160px; width: 175px;" class="fblike"><iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FNorthsideFest&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></div>
 
+<div style="left: -113px; margin-bottom: 0; margin-top: 0; padding-bottom: 0; position: relative; top: -202px; width: 175px;">
+        <script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1722826/06df53bc187a603ea0b692fc2675edbe/1402919/?v=a"> 
+    </script>
+    <div id="load_check" class="signup_form_message" >
+        This form needs Javascript to display, which your browser doesn't support. <a href="https://app.e2ma.net/app2/audience/signup/1722826/1402919/?v=a">    Sign up here</a> instead 
+    </div>
+    <script type="text/javascript">signupFormObj.drawForm();</script>
+
+    <div style="margin-left:685px; width:250px; font-size:10px; margin-top: -10px;">
+        SIGN UP FOR NORTHSIDE FESTIVAL UPDATES
+    </div>
+</div>
     <div id="primary"> 
         <div id="content" role="main">
 
